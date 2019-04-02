45ffbc8ab29a8be64cf2a7c0ca95249b94b93fa8
Fahmi Alfareza <fahmialfareza@icloud.com>
2019-04-02 13:52:45 +0700

    Initial Commit

---
+++ Lepas Suntuk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lepas Suntuk
+//
+//  Created by Fahmi Alfareza on 26/07/40 AH.
+//  Copyright © 1440 Fahmi Alfareza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
